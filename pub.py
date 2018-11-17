f7c614e0895c792532b48582e504c67cd9d6277e
leizhen-mbp <thunderlz@163.com>
2018-11-18 00:37:48 +0800

    翻译

--- pub.py
+++ pub.py
@@ -11,11 +11,12 @@ class pachong():
             , 'Accept-Language': 'zh-CN,zh;q=0.9'
             , 'Accept-Encoding': 'gzip, deflate'
             # ,'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
-            ,'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36'
+            ,'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
             , 'Host': host
             , 'Connection': 'keep-alive'
             , 'Cache-Control': 'max-age=0'
-            , 'Upgrade-Insecure-Requests': '1'}
+            , 'Upgrade-Insecure-Requests': '1'
+            ,'Cookie': 'PHOENIX_ID=0a650c81-154a0633f47-a97843; _hc.v="\"e27e18eb-3a3d-4b40-b06a-cbe624c96048.1462979739\""; s_ViewType=10; JSESSI aburl=1; cy=2; cye=beijing'}
 
     def getHtml(self,url, params=''):
         s = requests.Session()
